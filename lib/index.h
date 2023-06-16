6a20e800af479a81eef1a170de15ebc5878b012a
Ali Yassine <84604171+ali-yasine@users.noreply.github.com>
2023-06-17 00:49:38 +0300

    Update index.h

--- lib/index.h
+++ lib/index.h
@@ -4,7 +4,6 @@
 void getVertexInformativeness(const CsrGraph* csr, const CooGraph* coo, float* vertex_w, float& avg_hops);
 float averageDistanceInGraph(const CsrGraph* graph, const int num_samples = 1000);
 void getVertexInformativeness(const CsrGraph* csr, float* vertex_w);
-float averageDistanceInGraph(const CsrGraph* graph, const int num_samples = 10000);
 void writeGraphIndex(const char* filename, float* vertex_w, float avg_hops, int graph_size);
 void readGraphIndex(const char* filename, float* vertex_w, float& avg_hops, int graph_size);
 
